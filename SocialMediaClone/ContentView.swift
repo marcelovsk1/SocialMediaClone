3d926466e3f81fea2d5e1f8e7f834be627e56b72
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-08 13:36:13 -0300

    Initial Commit

---
+++ SocialMediaClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialMediaClone
+//
+//  Created by Marcelo Amaral Alves on 2023-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialMediaClone/SocialMediaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaCloneApp.swift
+//  SocialMediaClone
+//
+//  Created by Marcelo Amaral Alves on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
