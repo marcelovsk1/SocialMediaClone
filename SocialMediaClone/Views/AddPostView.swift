af41135cb060bf24d654b659ff655f5061385d2e
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-09 15:00:54 -0300

    adding new update to addpostview

--- SocialMediaClone/Views/AddPostView.swift
+++ SocialMediaClone/Views/AddPostView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct AddPostView: View {
     @Environment(\.dismiss) var dismiss
@@ -24,9 +25,54 @@ struct AddPostView: View {
                     .font(.system(size: 40))
                     .foregroundColor(.indigo)
                 TextField("Share your thoughts...", text: $postTitle)
+                    .padding(10)
+                    .frame(maxWidth: .infinity)
             }
             .padding(.horizontal, 20)
             .padding(.vertical, 10)
+            
+            Button {
+                isShowingImagePicker.toggle()
+            } label: {
+                VStack {
+                    if let image = selectedImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                    } else {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .font(.system(size: 50))
+                            .frame(width: 300, height: 300)
+                            .foregroundColor(.indigo)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.gray, lineWidth: 1)
+                            )
+                    }
+                }
+            }
+            .padding()
+            
+            Text(errorMessage)
+                .bold()
+                .foregroundColor(.red)
+            
+            Button {
+                if selectedImage == nil {
+                    errorMessage = "You must select an image first"
+                    return
+                }
+                
+                
+                } label: {
+                    
+            }
         }
         .cornerRadius(20)
         .padding(.horizontal, 20)
