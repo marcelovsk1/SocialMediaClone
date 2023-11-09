e5a83f6015f60a9f35312970897ecaa816fb48b9
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-09 14:17:52 -0300

    creating addpostview

---
+++ SocialMediaClone/Views/AddPostView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPostView.swift
+//  SocialMediaClone
+//
+//  Created by Marcelo Amaral Alves on 2023-11-09.
+//
+
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPostView()
+}
