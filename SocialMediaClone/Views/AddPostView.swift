04249745b84d72b966260c0f57be3796c8d9fcbc
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-09 14:46:49 -0300

    working on addpostview

--- SocialMediaClone/Views/AddPostView.swift
+++ SocialMediaClone/Views/AddPostView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct AddPostView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var postTitle = ""
+    @State private var selectedImage: UIImage?
+    @State private var isShowingImagePicker = false
+    @State private var errorMessage = ""
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
