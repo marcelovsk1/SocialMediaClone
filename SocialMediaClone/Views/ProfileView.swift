72450f01c4443277e41128ca42ae0c9f306a7905
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-09 11:18:38 -0300

    working on profileview

--- SocialMediaClone/Views/ProfileView.swift
+++ SocialMediaClone/Views/ProfileView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @ObservedObject var loginViewModel: LoginViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("OLA")
     }
 }
 
 #Preview {
-    ProfileView()
+    ProfileView(loginViewModel: LoginViewModel())
 }
