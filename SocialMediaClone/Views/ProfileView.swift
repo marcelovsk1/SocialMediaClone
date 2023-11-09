b5ce15f56ef880ee54e2cef7b00f129a1f2822fc
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-09 11:27:49 -0300

    signOut function implemented

--- SocialMediaClone/Views/ProfileView.swift
+++ SocialMediaClone/Views/ProfileView.swift
@@ -10,10 +10,33 @@ import SwiftUI
 struct ProfileView: View {
     @ObservedObject var loginViewModel: LoginViewModel
     
+    @State private var showLogOutOptions = false
+    
     var body: some View {
         NavigationView {
             VStack {
                 
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        showLogOutOptions.toggle()
+                    } label: {
+                        Image(systemName: "gear")
+                            .font(.system(size: 24))
+                            .bold()
+                            .foregroundColor(.indigo)
+                    }
+                }
+            }
+            .actionSheet(isPresented: $showLogOutOptions) {
+                ActionSheet(title: Text("Settings"),
+                message: Text("What do you want to do?"),
+                            buttons: [.destructive(Text("Sign out"),
+                                                   action: {
+                    loginViewModel.handleSignOut()
+                }), .cancel()
+                            ])
             }
         }
     }
