1d8ade40dd9228c02175ae28e1b60f4d07652cf5
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-10 15:56:27 -0300

    working on

--- SocialMediaClone/Models/ProfileViewModel.swift
+++ SocialMediaClone/Models/ProfileViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Firebase
+
+class ProfileViewModel: ObservableObject {
+    @Published var post = [Post]()
+    
+    init() {
+        getUserPosts()
+        
+    }
+    
+    func getUserPosts() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        FirebaseManager.shared.firestore
+            .collection("posts")
+            .whereField("useruid", isEqualTo: uid)
+            .getDocuments { documentsSnapshot, error in
+                if let error = error {
+                    print("Failed to fetch posts \(error)")
+                    return
+                }
+                
+                
+            }
+    }
+}
