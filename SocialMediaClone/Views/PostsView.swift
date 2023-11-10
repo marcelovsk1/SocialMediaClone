b02875e5d79a1032bc745a5a32ae626ff232e5af
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-10 14:41:36 -0300

    working on postsview

--- SocialMediaClone/Views/PostsView.swift
+++ SocialMediaClone/Views/PostsView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct PostComponent: View {
+    let post: Post
+    var postsViewModel = PostsViewModel()
+    @State private var profileImage: UIImage?
+    @State private var isLoadingImage = false
+    
+    var body: some View {
+        
+    }
+}
+
 struct PostsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
