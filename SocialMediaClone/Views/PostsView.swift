4bd81e183cc6498562628a59eaa8fc704c6e4c79
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-10 14:48:05 -0300

    new update

--- SocialMediaClone/Views/PostsView.swift
+++ SocialMediaClone/Views/PostsView.swift
@@ -14,7 +14,50 @@ struct PostComponent: View {
     @State private var isLoadingImage = false
     
     var body: some View {
-        
+        VStack {
+            Divider()
+                .padding(.horizontal)
+            
+            HStack {
+                if isLoadingImage {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipped()
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 44)
+                                .stroke(Color(.label), lineWidth: 1)
+                        )
+                        .padding(.leading)
+                } else if let image = profileImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipped()
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 44)
+                                .stroke(Color(.label), lineWidth: 1)
+                            )
+                        .padding(.leading)
+                }
+                
+                Text(post.name)
+                    .padding(.vertical, 2)
+                    .padding(.horizontal)
+                    .foregroundColor(.indigo)
+                
+                Spacer()
+            }
+        }
+        .onAppear {
+            isLoadingImage = true
+            postsViewModel.getUserProfileImage(userUID: post.userUID) { image in
+                profileImage = image
+                isLoadingImage = false
+            }
+        }
     }
 }
 
