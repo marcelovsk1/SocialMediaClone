c5bcda0e4386ba6693cd9a16a922a7908ba30d7d
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-10 15:20:48 -0300

    fixing some bugs

--- SocialMediaClone/Views/SearchView.swift
+++ SocialMediaClone/Views/SearchView.swift
@@ -9,12 +9,36 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var text = ""
-    @ObservableObject var postsViewModel: PostsViewModel
+    @ObservedObject var postsViewModel: PostsViewModel
     
     var body: some View {
         VStack {
             HStack {
+                withAnimation(.default) {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .padding(.leading, 8)
+                        
+                        TextField("Search", text: $text)
+                    }
+                    .padding(.horizontal, 8)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    
+                    
+                }
                 
+                if !text.isEmpty {
+                    Button {
+                        text = ""
+                    } label: {
+                        Text("Cancel")
+                            .foregroundColor(.indigo)
+                    }
+                    .padding(.trailing, 8)
+                    .transition(.move(edge: .trailing))
+                }
             }
             .transition(.move(edge: .top))
         }
