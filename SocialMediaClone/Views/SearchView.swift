8be9b123dc3fd08eeba8d5309f33bbfaf8fa200f
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-10 15:06:39 -0300

    creating searchview

---
+++ SocialMediaClone/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  SocialMediaClone
+//
+//  Created by Marcelo Amaral Alves on 2023-11-10.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
